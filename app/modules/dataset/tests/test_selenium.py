58b8b4715340e044d1635ebc2076ca8fde3a480c
Ricardo Carreño Mariño <riccarmar@alum.us.es>
2024-12-17 14:16:09 +0100

    test(dataset): new selenium test and patch in view dataset

--- app/modules/dataset/tests/test_selenium.py
+++ app/modules/dataset/tests/test_selenium.py
@@ -4,7 +4,9 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+from core import selenium
 from core.environment.host import get_host_for_selenium_testing
 from core.selenium.common import initialize_driver, close_driver
 
@@ -208,6 +210,104 @@ def test_upload_anonymous_dataset():
         # Close the browser
         close_driver(driver)
 
+def test_make_dataset_public():
+    driver = initialize_driver()
+    i = int(time.time())
+
+    try:
+        host = get_host_for_selenium_testing()
+
+        # Open the login page
+        driver.get(f"{host}/login")
+        wait_for_page_to_load(driver)
+
+        # Find the username and password field and enter the values
+        email_field = driver.find_element(By.NAME, "email")
+        password_field = driver.find_element(By.NAME, "password")
+
+        email_field.send_keys("user1@example.com")
+        password_field.send_keys("1234")
+
+        # Send the form
+        password_field.send_keys(Keys.RETURN)
+        wait_for_page_to_load(driver)
+
+        # Count initial datasets
+        initial_datasets = count_datasets(driver, host)
+
+        # Open the upload dataset
+        driver.get(f"{host}/dataset/upload")
+        wait_for_page_to_load(driver)
+
+        # Find basic info and UVL model and fill values
+        title_field = driver.find_element(By.NAME, "title")
+        title_field.send_keys(f"Prueba hacer publico {i}")
+        desc_field = driver.find_element(By.NAME, "desc")
+        desc_field.send_keys("Description")
+        tags_field = driver.find_element(By.NAME, "tags")
+        tags_field.send_keys("tag1,tag2")
+
+        # Obtén las rutas absolutas de los archivos
+        file1_path = os.path.abspath("app/modules/dataset/uvl_examples/file1.uvl")
+        file2_path = os.path.abspath("app/modules/dataset/uvl_examples/file2.uvl")
+
+        # Subir el primer archivo
+        dropzone = driver.find_element(By.CLASS_NAME, "dz-hidden-input")
+        dropzone.send_keys(file1_path)
+        wait_for_page_to_load(driver)
+
+        # Subir el segundo archivo
+        dropzone = driver.find_element(By.CLASS_NAME, "dz-hidden-input")
+        dropzone.send_keys(file2_path)
+        wait_for_page_to_load(driver)
+
+        # Check Anonymous box
+        anonymous_check = driver.find_element(By.ID, "anonymousCheckbox")
+        anonymous_check.send_keys(Keys.SPACE)
+        wait_for_page_to_load(driver)
+
+        # Check I agree and send form
+        check = driver.find_element(By.ID, "agreeCheckbox")
+        check.send_keys(Keys.SPACE)
+        wait_for_page_to_load(driver)
+
+        upload_btn = driver.find_element(By.ID, "upload_button")
+        upload_btn.send_keys(Keys.RETURN)
+        wait_for_page_to_load(driver)
+        time.sleep(2)  # Force wait time
+
+        # Open the newly uploaded dataset page
+        driver.find_element(By.XPATH, f"//a[contains(text(), 'Prueba hacer publico {i}')]").click()
+        wait_for_page_to_load(driver)
+
+        # Make dataset public
+        make_public = driver.find_element(By.XPATH, "//button[contains(text(), 'Make authors metadata public')]")
+        make_public.send_keys(Keys.RETURN)
+
+        # Handle the alert
+        WebDriverWait(driver, 5).until(EC.alert_is_present())  # Wait for the alert to appear
+        alert = driver.switch_to.alert
+        assert "Success: Dataset anonymity toggled successfully" in alert.text, "Unexpected alert text!"
+        alert.accept()  # Accept (close) the alert
+        wait_for_page_to_load(driver)
+
+        # Verify that the dataset is not marked as anonymous
+        uploaded_by = driver.find_element(By.ID, "uploadedby")
+        assert "Anónimo" not in uploaded_by.text, "Dataset is marked as anonymous in 'Uploaded by' section!"
+
+        authors = driver.find_elements(By.ID, "authors")
+        for author in authors:
+            assert "Anónimo" not in author.text, "An author is marked as anonymous!"
+
+        print("Test passed!")
+
+    finally:
+        # Close the browser
+        close_driver(driver)
+
+
 
 # Call the test function
-test_upload_dataset()
+# test_upload_dataset()
+# test_upload_anonymous_dataset()
+# test_make_dataset_public()
